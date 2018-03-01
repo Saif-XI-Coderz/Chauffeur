8c81c7a535084d5b7d32952c574b12a444d7e15b
SaifSohomow <saifsohomow@gmail.com>
2018-03-02 00:49:27 +0400

    calc distance

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ let IO = require("./IO.js");
 
 async function bootstrap(){
   var {parameters, rides} = await IO.read(process.argv[2]);
-
   console.info("parameters", parameters);
   console.info("rides", rides);
-
-    for(var i =0; i<rides.length; i++) {
-        console.log(calcDistance(parseInt(rides[i]['y'] )+ parseInt(rides[i]['x']), (parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']))));
-        //console.log(Math.abs((parseInt(rides[i]['y'] )+ parseInt(rides[i]['x'])) - (parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']))));
-    }
+  console.info(rides.map(ride => distance({r:ride.a,c:ride.b},{r:ride.x,c:ride.y})));
 }
-function calcDistance (objA, objB) {
 
-    return Math.abs(objA-objB);
+function distance(start, end) {
+  return Math.abs(start.r-end.r) + Math.abs(start.c-end.c);
 }
 
 bootstrap();
