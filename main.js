93b56463d599236055a7a3b1fc399796cfb50761
Arvind <balgo_arvind@hotmail.com>
2018-03-02 00:35:51 +0400

    test arvind

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ async function bootstrap(){
 
   console.info("parameters", parameters);
   console.info("rides", rides);
+
+    for(var i =0; i<rides.length; i++) {
+        console.log(calcDistance(parseInt(rides[i]['y'] )+ parseInt(rides[i]['x']), (parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']))));
+        //console.log(Math.abs((parseInt(rides[i]['y'] )+ parseInt(rides[i]['x'])) - (parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']))));
+    }
+}
+function calcDistance (objA, objB) {
+
+    return Math.abs(objA-objB);
 }
 
 bootstrap();
