c15d40295daed5fbf1e975388ec73f0c97732c2c
SaifSohomow <saifsohomow@gmail.com>
2018-03-02 01:09:41 +0400

    get nearest ride

--- main.js
+++ main.js
@@ -4,33 +4,18 @@ async function bootstrap(){
   var {parameters, rides} = await IO.read(process.argv[2]);
   console.info("parameters", parameters);
   console.info("rides", rides);
-  console.info(rides.map(ride => distance({r:ride.a,c:ride.b},{r:ride.x,c:ride.y})));
+  console.info(rides.map(ride => getDistance({r:ride.a,c:ride.b},{r:ride.x,c:ride.y})));
+  console.info(nearestPickup({r:0,c:3}, rides))
 }
 
-function distance(start, end) {
+function getDistance(start, end) {
   return Math.abs(start.r-end.r) + Math.abs(start.c-end.c);
 }
 
-function getClosesDist(x, y, rides) {
-    console.log(x, y)
-
-    var distOrig = calcDistance(x, y);
-    var objCoordinateNext = {a:'',b:''};
-    var distance = 0;
-    for(var i =0; i<rides.length; i++) {
-        if(distance == 0) {
-            distance = calcDistance((parseInt(x) + parseInt(y)) , parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']));
-            objCoordinateNext['a'] = rides[i]['a'];
-            objCoordinateNext['b'] = rides[i]['b'];
-        }
-        if(calcDistance((parseInt(x) + parseInt(y)) , parseInt(rides[i]['a'] )+ parseInt(rides[i]['b'])) < distance) {
-            distance = calcDistance((parseInt(x) + parseInt(y)) , parseInt(rides[i]['a'] )+ parseInt(rides[i]['b']));
-            objCoordinateNext['a'] = rides[i]['a'];
-            objCoordinateNext['b'] = rides[i]['b'];
-        }
-    }
-
-    console.log(objCoordinateNext)
+function nearestPickup(position, rides){
+  return rides.reduce(
+    (nearestRide,ride) => getDistance({r:ride.a,c:ride.b},position) < getDistance({r:nearestRide.a,c:nearestRide.b},position) ? ride : nearestRide
+  );
 }
 
 bootstrap();
