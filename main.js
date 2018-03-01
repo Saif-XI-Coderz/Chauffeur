731e219fc7bdb1f548c7ff61fa94013bdae22f19
Arvind <balgo_arvind@hotmail.com>
2018-03-02 01:16:02 +0400

    test arvind

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 let IO = require("./IO.js");
 
-async function bootstrap(){
-  var {parameters, rides} = await IO.read(process.argv[2]);
-  console.info("parameters", parameters);
-  console.info("rides", rides);
-  console.info(rides.map(ride => getDistance({r:ride.a,c:ride.b},{r:ride.x,c:ride.y})));
-  console.info(nearestPickup({r:0,c:3}, rides))
+async
+function bootstrap() {
+    var {parameters, rides} = await
+    IO.read(process.argv[2]);
+    console.info("parameters", parameters);
+    console.info("rides", rides);
+    console.info(rides.map(ride = > getDistance({r: ride.a, c: ride.b}, {r: ride.x, c: ride.y}))
+)
+    ;
+    console.info(nearestPickup({r: 0, c: 3}, rides))
 }
 
 function getDistance(start, end) {
-  return Math.abs(start.r-end.r) + Math.abs(start.c-end.c);
+    return Math.abs(start.r - end.r) + Math.abs(start.c - end.c);
 }
 
-function nearestPickup(position, rides){
-  return rides.reduce(
-    (nearestRide,ride) => getDistance({r:ride.a,c:ride.b},position) < getDistance({r:nearestRide.a,c:nearestRide.b},position) ? ride : nearestRide
-  );
+function nearestPickup(position, rides) {
+    return rides.reduce(
+        (nearestRide, ride) = > getDistance({r: ride.a, c: ride.b}, position) < getDistance({
+        r: nearestRide.a,
+        c: nearestRide.b
+    }, position) ? ride : nearestRide
+)
+    ;
 }
 
-function getFinishPoint(ride){
-  return { r:ride.x, c:ride.y};
+function getFinishPoint(ride) {
+    return {r: ride.x, c: ride.y};
+}
+
+function getStartPoint(ride) {
+    return {r: ride.a, c: ride.b};
 }
 
 bootstrap();
