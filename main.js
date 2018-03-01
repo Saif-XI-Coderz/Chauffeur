ff64e973b2054f55ab06d070ca4b7f71cd58518f
SaifSohomow <saifsohomow@gmail.com>
2018-03-02 01:20:09 +0400

    refactor

--- main.js
+++ main.js
@@ -6,24 +6,18 @@ function bootstrap() {
     IO.read(process.argv[2]);
     console.info("parameters", parameters);
     console.info("rides", rides);
-    console.info(rides.map(ride = > getDistance({r: ride.a, c: ride.b}, {r: ride.x, c: ride.y}))
-)
-    ;
+    console.info(rides.map(ride = > getDistance(getStartPoint(ride), getFinishPoint(ride))));
     console.info(nearestPickup({r: 0, c: 3}, rides))
 }
 
 function getDistance(start, end) {
-    return Math.abs(start.r - end.r) + Math.abs(start.c - end.c);
+  return Math.abs(start.r - end.r) + Math.abs(start.c - end.c);
 }
 
 function nearestPickup(position, rides) {
-    return rides.reduce(
-        (nearestRide, ride) = > getDistance({r: ride.a, c: ride.b}, position) < getDistance({
-        r: nearestRide.a,
-        c: nearestRide.b
-    }, position) ? ride : nearestRide
-)
-    ;
+  return rides.reduce(
+    (nearestRide, ride) = > getDistance(getStartPoint(ride) , position) < getDistance(getStartPoint(nearestRide), position) ? ride : nearestRide
+  );
 }
 
 function getFinishPoint(ride) {
